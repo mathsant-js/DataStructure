c5a9653b3d19ed443cfeead5a1e48b4c6fa2de1d
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2026-04-15 20:44:53 -0300

    feat: trabalhando com else if

--- semana3_condicionaisRepeticao/main.c
+++ semana3_condicionaisRepeticao/main.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int idade = 18;
 
-    if (idade >= 18) {
+    if (idade >= 18)
+    {
         printf("Maior de idade!\n");
-    } else {
+    }
+    else
+    {
         printf("Menor de idade!\n");
     }
+
+    // Mostrando Média e Ausência
+    int media;
+    int presenca;
+
+    printf("Média: %d | Presença: %d\n", media, presenca);
+
+    if (media >= 9 && presenca >= 75)
+    {
+        printf("Excelente aprovado!\n");
+    }
+    else if (media >= 6 && presenca >= 75)
+    {
+        printf("Aprovado!\n");
+    }
+    else if (media < 6 && presenca >= 75)
+    {
+        printf("Reprovado, média insuficiente!");
+    }
+    else {
+        printf("Reprovado, excesso de faltas!");
+    }
+    
 
     return 0;
 }
