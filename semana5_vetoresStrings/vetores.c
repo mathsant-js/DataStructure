da38214719aae7c5671d855a6f875129713e635d
mathsant-js <matheus.santana114@etec.sp.gov.br>
2026-04-30 08:57:20 -0300

    style: ajeitando a ordem das funções no código

--- semana5_vetoresStrings/vetores.c
+++ semana5_vetoresStrings/vetores.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
-
-// Função para percorrer o vetor
-void mostrar_vetor(int vetor[]) {
-    for (int i = 0; i < 3; i++) {
-        printf("vetor[%d] = %d\n", i, vetor[i]);
-    }
-}
+void mostrar_vetor(int vetor[]);
 
 int main() {
     int valores[3] = {1, 2, 3};
@@ -20,4 +14,11 @@ int main() {
     mostrar_vetor(valores);
 
     return 0;
+}
+
+// Função para percorrer o vetor
+void mostrar_vetor(int vetor[]) {
+    for (int i = 0; i < 3; i++) {
+        printf("vetor[%d] = %d\n", i, vetor[i]);
+    }
 }
