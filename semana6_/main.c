a5f31101c626cade3b5277a9ce767005152d23b0
mathsant-js <matheus.santana114@etec.sp.gov.br>
2026-05-07 09:11:16 -0300

    feat: trabalhando com tratamento de string e funções novas

--- semana6_/main.c
+++ semana6_/main.c
@@ -10,13 +10,15 @@ int main() {
     printf("Digite o seu nome completo: ");
     // scanf("%s", nome);
 
-    // Consegue capturar o espaço digitado
-    fgets(nome, 20, stdin);
+    // Consegue capturar o nome digitado
+    // (variavel, tamanho, teclado)
+    // stdin ele diz que o valor será digitado pelo teclado
+    fgets(nome, sizeof(nome), stdin);
     int tamanho_antes = strlen(nome);
 
     // Capturo o espaço para e coloco ele para encerrar o vetor, assim, no final ele não pula linha
     // Quando retiro o \n o tamanho do vetor diminui em 1
-    nome[strcspn(nome, "\n")] = '\0';
+    nome[strcspn(nome, " \n")] = '\0';
 
     int tamanho_depois = strlen(nome); // tamanho final após strcsnp da string
 
@@ -24,5 +26,18 @@ int main() {
     printf("\nTamanho Antes: %d", tamanho_antes);
     printf("\nTamanho Depois: %d", tamanho_depois);
 
+    char nome_2[7];
+    strcpy(nome_2, nome);
+    
+    printf("\nComo posso te ajudar, %s", nome_2);
+
+    printf("\n");
+
+    // Adição de Mensagem
+
+    char msg[20] = "Como posso ajudar?";
+    //printf("%s. %s\n", nome_2, msg);
+    puts(nome_2);
+    puts(msg);
     return 0;
 }
