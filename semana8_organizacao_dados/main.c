a99a615b2c9fb81db6d1afaf6325c41644e33988
mathsant-js <matheus.santana114@etec.sp.gov.br>
2026-08-20 08:49:51 -0300

    feat: adicionando inputs para alunos

--- semana8_organizacao_dados/main.c
+++ semana8_organizacao_dados/main.c
@@ -25,5 +25,22 @@ int main()
     printf("Nome: %s\n", aluno[0].nome);
     printf("Nota: %f", aluno[0].nota);
 
+    printf("\n\n");
+
+    aluno[1].rm = 789455;
+
+    printf("Digite o nome do aluno: ");
+    scanf("%s", aluno[1].nome);
+
+    printf("Digite a nota do aluno: ");
+    scanf("%f", &aluno[1].nota);
+
+    printf("\n\n");
+
+    printf("==== INFO ALUNO ====\n");
+    printf("RM: %d\n", aluno[1].rm);
+    printf("Nome: %s\n", aluno[1].nome);
+    printf("Nota: %f", aluno[1].nota);
+
     return 0;
 }
