caad62a58123ea51a510af497caef7ac842633f9
mathsant-js <matheus.santana114@etec.sp.gov.br>
2026-08-27 09:49:44 -0300

    feat: desafio de atualizar valor em memória de produto concluído
    - valor em memória de produto é atualizado pela função aplicarDesconto com verificação de porcentagem válida

---
+++ semana9_ponteiros/desafio.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+typedef struct
+{
+    int id;
+    char nome[50];
+    float preco;
+} Produto;
+
+void aplicarDesconto(Produto *p, float desc);
+
+int main()
+{
+    Produto produto = {0, "Manteiga", 20.00};
+
+    Produto *ptr = &produto;
+
+    float porcentagem_desconto = 10;
+
+    printf("\n==== INFO PRODUTO ====\n");
+    printf("Nome: %s", ptr->nome);
+    printf("\nPreco Original: R$%.2f", ptr->preco);
+    printf("\nPorcentagem de desconto: %.2f%%", porcentagem_desconto);
+
+    aplicarDesconto(ptr, porcentagem_desconto);
+
+    printf("\nPreco com Desconto: R$%.2f", ptr->preco);
+}
+
+void aplicarDesconto(Produto *p, float desc)
+{
+    desc = (desc / 100) * p->preco;
+
+    p->preco = p->preco - desc;
+}
