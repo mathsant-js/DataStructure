e15ee90a0a5fd9e9d1a0c764e94c6aee99698060
mathsant-js <matheus.santana114@etec.sp.gov.br>
2026-08-27 09:33:24 -0300

    feat: criando função para alterar valor de nota em memória do argumento da função

--- semana9_ponteiros/main.c
+++ semana9_ponteiros/main.c
@@ -7,6 +7,8 @@ typedef struct
     float nota;
 } Aluno;
 
+void atualizar_nota(Aluno* aluno);
+
 int main() {
     // Testando com um aluno
     Aluno a1 = {123, "Matheus", 5};
@@ -34,4 +36,15 @@ int main() {
     printf("RM: %d\n", ponteiro->rm);
     printf("Nome: %s\n", ponteiro->nome);
     printf("Nota: %.2f\n", ponteiro->nota);
+
+    // Atualizando nota do aluno
+    atualizar_nota(ptr);
+
+    printf("\nNome: %s\n", ptr->nome);
+    printf("Nota: %f", ptr->nota);
+}
+
+// Criando função que altera valor em memória do argumento
+void atualizar_nota(Aluno* aluno) {
+    aluno->nota = 10;
 }
