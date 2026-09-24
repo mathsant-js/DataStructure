849b0ce3a94542a2fcd23b12d3bbb34db4eb89d1
mathsant-js <matheus.santana114@etec.sp.gov.br>
2026-09-24 09:00:01 -0300

    feat: inserindo novo início na struct

--- semana12_tiposAbstratosDados/listas_ligadas/node.c
+++ semana12_tiposAbstratosDados/listas_ligadas/node.c
@@ -7,6 +7,13 @@ struct Node
     struct Node *proximo; // ponteiro -> O link
 };
 
+void inserirInicio(struct Node** head, int valor) {
+    struct Node* novo = malloc(sizeof(struct Node));
+    novo->dado = valor;
+    novo->proximo = *head;
+    *head = novo;
+}
+
 int main() {
     struct Node* No;
 
@@ -21,6 +28,20 @@ int main() {
 
     // Depois de atribuir valor
     printf("%d\n", No->dado);
+
+    // Inserindo novo início
+    if (No == NULL) {
+        printf("[ERRO] Memoria insuficiente\n");
+        exit(1);
+    }
+
+    inserirInicio(&No, 5);
+
+    // Imprimindo novo início
+    printf("%d\n", No->dado);
+
+    // Imprimindo o novo próximo
+    printf("%d\n", No->proximo->dado);
 
     return 0;
 }
